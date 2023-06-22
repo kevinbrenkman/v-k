db9ddd3d07edb371bded010e2b6602fbebf100fe
kevinbrenkman <60971832+kevinbrenkman@users.noreply.github.com>
2023-06-22 18:42:33 +0200

    Update grid-view.js

--- grid-view.js
+++ grid-view.js
@@ -253,17 +253,12 @@ fadeInVideo(video);
     }
   }
 
-	function exitGridViewOnResize() {
-  if (window.innerWidth < 992 && isGridViewActive) {
-    exitGridView();
-  }
-}
+
 
   function initializeScript() {
     handleViewportChange();
     window.addEventListener('scroll', handleViewportChange);
     window.addEventListener('resize', handleViewportChange);
-  window.addEventListener('resize', exitGridViewOnResize); // Call exitGridViewOnResize on window resize
   }
 
  function applyPortraitImageWidth() {
